4da712be5736c499180981fa4d1861e7eb68902d
“sokyran” <karass0001@gmail.com>
2022-06-17 20:12:39 +0300

    refactor: update how query is parsed

--- frontend/src/utils/parse-query.js
+++ frontend/src/utils/parse-query.js
@@ -4,8 +4,9 @@ const parseQuery = (queryString) => {
   let query = {};
   let pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
   for (let i = 0; i < pairs.length; i++) {
-      let pair = pairs[i].split('=');
-      query[pair[0]] = htmlSanitize(pair[1] || '');
+      let [param, ...rest] = pairs[i].split('=')
+      let value = rest.join('=')
+      query[param] = (value || '');
   }
   return query;
 };
